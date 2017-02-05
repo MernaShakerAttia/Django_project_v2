efbd2f91ae0980a10a9270492be4551f25e75642
ahmed <elmahdy30@gmail.com>
2017-02-05 03:46:01 +0200

    with new update databases and created admin

--- groupA/geeks/models.py
+++ groupA/geeks/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 from datetime import date
+from PIL import Image
 
 
 # Create your models here.
@@ -13,6 +14,8 @@ from datetime import date
 	passwd=models.CharField(max_length=200)#)"""
 class category(models.Model):
 	title=models.CharField(max_length=200)
+	def __str__(self):
+		return self.title
 
 class Post(models.Model):
 	title=models.CharField(max_length = 200)
@@ -20,6 +23,12 @@ class Post(models.Model):
 	post_category=models.ForeignKey(category,on_delete=models.CASCADE)
 	user_reply=models.TextField()
 	post_time=models.DateTimeField('datetime')
+	post_image=models.ImageField(upload_to='/tmp')
+	def __str__(self):
+		return '%s %s %s '%(self.title, self.post_category, self.category)
+class Admin(models.Model):
+	pass
+
 	
 
 
@@ -28,6 +37,9 @@ class Post(models.Model):
 
 class forbidden(models.Model):
 	rude_words=models.TextField()
+	def __str__(self):
+		return self.rude_words
+
 
 
 	
