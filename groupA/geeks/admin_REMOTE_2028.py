4f2e8f87e97208861a317d17203b6ee3b5772bf1
ahmed <elmahdy30@gmail.com>
2017-02-07 21:12:39 +0200

    deleting progict

--- groupA/geeks/admin_REMOTE_2028.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import category, forbidden, users,Post,Comment
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(category)
-admin.site.register(users)
-admin.site.register(forbidden)
